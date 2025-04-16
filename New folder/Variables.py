525ee2d3cf00b40e87bd116a75e0690364b13ffd
js0554146 <js0554146@gmail.com>
2025-04-15 20:15:29 -0400

    Birthdays

---
+++ New folder/Variables.py
@@ -0,0 +1,11 @@
+Cruz = "April 17th"
+Tyler = "Jaunary 24th"
+Cali = "April 4th"
+Alannah = "May 6th"
+Matthew = "October 11th"
+
+print(Cruz)
+print(Cali)
+print(Tyler)
+print(Matthew)
+print(Alannah)
